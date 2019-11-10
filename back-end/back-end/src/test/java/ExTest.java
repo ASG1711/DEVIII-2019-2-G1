71509b41cadb31f19e871b5154e1e81e0d58bed3
ASG1711 <andgoularte@hotmail.com>
2019-11-10 10:54:06 -0300

    Exemplo Teste Selenium

---
+++ back-end/back-end/src/test/java/ExTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ *
+ * @author Anderson
+ */
+public class ExTest {
+    
+    @Test
+    public void testSimple() throws Exception {
+        // Create a new instance of the Chrome driver
+        // Notice that the remainder of the code relies on the interface, 
+        // not the implementation.
+        WebDriver driver = new ChromeDriver();
+
+        // And now use this to visit NetBeans
+        driver.get("http://www.netbeans.org");
+        // Alternatively the same thing can be done like this
+        // driver.navigate().to("http://www.netbeans.org");
+
+        // Check the title of the page
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().contains("NetBeans");
+            }
+        });
+
+        //Close the browser
+        driver.quit();
+    }
+    
+}
